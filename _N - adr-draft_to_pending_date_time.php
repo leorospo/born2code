8309e8994bf8e6cf5255c1a77667e5b37a3e5a08
leorospo <37043425+leorospo@users.noreply.github.com>
2018-11-25 15:06:43 +0100

    Create _N - adr-draft_to_pending_date_time.php

---
+++ _N - adr-draft_to_pending_date_time.php
@@ -0,0 +1,16 @@
+			//Draft to pending adds
+function draft_to_pending_date_time_updater( $post ) {
+	
+	if ('racconti' == $post->post_type) {
+		$meta_value = current_time( 'Y-m-d H:i:s' );
+		update_post_meta( $post->ID, 'draft_to_pending_date_time', $meta_value );
+	}
+	
+};
+add_action(  'draft_to_pending',  'draft_to_pending_date_time_updater' );
+
+			//Any state to draft removes
+function racconti_to_draft_date_time_deleter( $ID, $post ) {
+	update_post_meta( $ID, 'draft_to_pending_date_time', '' );
+}
+add_action(  'draft_racconti',  'racconti_to_draft_date_time_deleter', 10, 2 );
