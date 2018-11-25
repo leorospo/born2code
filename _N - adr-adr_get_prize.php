082ad05cdf0541c10b66a27f0db4f3df40ebface
leorospo <37043425+leorospo@users.noreply.github.com>
2018-11-25 15:03:27 +0100

    Create _N - adr-adr_get_prize.php

---
+++ _N - adr-adr_get_prize.php
@@ -0,0 +1,34 @@
+function adr_get_prize ($draft_to_pending_date_time) {
+	
+	$date = $draft_to_pending_date_time;
+	
+	if ( empty($date) ) {
+		return('');
+		
+	} elseif ( get_option( 'data-inizio-2019' ) <= $date && $date <= get_option( 'data-fine-2019' ) ) {
+		return('2019');
+		
+	} elseif( get_option( 'data-fine-2019' ) <= $date && $date <= get_option( 'data-fine-2020' ) ) {
+		return('2020');
+		
+	} elseif( get_option( 'data-fine-2020' ) <= $date && $date <= get_option( 'data-fine-2021' ) ) {
+		return('2021');
+		
+	} elseif( get_option( 'data-fine-2021' ) <= $date && $date <= get_option( 'data-fine-2022' ) ) {
+		return('2022');
+		
+	} elseif( get_option( 'data-fine-2022' ) <= $date && $date <= get_option( 'data-fine-2023' ) ) {
+		return('2023');
+		
+	} elseif( get_option( 'data-fine-2023' ) <= $date && $date <= get_option( 'data-fine-2024' ) ) {
+		return('2024');
+		
+	} elseif( get_option( 'data-fine-2024' ) <= $date && $date <= get_option( 'data-fine-2025' ) ) {
+		return('2025');
+
+	} else {
+		return('Error -> functions.php -> adr_get_prize()');
+		
+	}
+	
+}
